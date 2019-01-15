b3bdc4cfa8a4428d65fefedf38d599bc2ec27fc9
kavindu <kavindupath@gmail.com>
2019-01-15 19:43:00 +0530

    Commit All

---
+++ TextToSpeech/ReadForME.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace TextToSpeech
+{
+    partial class ReadForME
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(485, 275);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnStart.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStart.Location = new System.Drawing.Point(12, 351);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(107, 34);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnPause.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPause.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPause.Location = new System.Drawing.Point(138, 351);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(112, 34);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnResume.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResume.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnResume.Location = new System.Drawing.Point(12, 391);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(107, 34);
+            this.btnResume.TabIndex = 3;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = false;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnStop.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStop.Location = new System.Drawing.Point(138, 391);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(112, 34);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TextToSpeech.Properties.Resources.download;
+            this.pictureBox1.Location = new System.Drawing.Point(408, 366);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ReadForME
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(492, 452);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ReadForME";
+            this.Text = "ReadForME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TextToSpeech/ReadForME.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace TextToSpeech
+{
+    public partial class ReadForME : Form
+    {
+        public ReadForME()
+        {
+            InitializeComponent();
+            
+        }
+        SpeechSynthesizer speechSynthesizerObj;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             speechSynthesizerObj=new SpeechSynthesizer();
+            btnPause.Enabled = false;
+            btnResume.Enabled = false;
+            btnStop.Enabled = false;
+            
+           
+
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            speechSynthesizerObj.Dispose();  //Disposes the SpeechSynthesizer object
+            if (richTextBox1.Text != "")
+            {
+              speechSynthesizerObj = new SpeechSynthesizer();
+                speechSynthesizerObj.SelectVoiceByHints(VoiceGender.Female);
+
+                speechSynthesizerObj.SpeakAsync(richTextBox1.Text);
+                btnPause.Enabled = true;
+                btnStop.Enabled = true;
+               
+
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if(speechSynthesizerObj!=null)
+                {
+                if(speechSynthesizerObj.State==SynthesizerState.Speaking)  //Gets the current speaking state of the SpeechSynthesizer object.  
+                {
+                    speechSynthesizerObj.Pause();
+                    btnResume.Enabled = true;
+                    btnStart.Enabled = false;
+
+                }
+
+            }
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            if(speechSynthesizerObj!=null)
+            {
+                if (speechSynthesizerObj.State == SynthesizerState.Paused)
+                {
+                    speechSynthesizerObj.Resume();
+                    btnResume.Enabled = false;
+                    btnStart.Enabled = true;
+
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if(speechSynthesizerObj!=null)
+            {
+                speechSynthesizerObj.Dispose();
+                btnStart.Enabled = true;
+                btnResume.Enabled = false;
+                btnPause.Enabled = false;
+                btnStop.Enabled = false;
+            }
+        }
+
+       
+    }
+}
